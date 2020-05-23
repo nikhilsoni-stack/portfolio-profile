b22fd4ebe071c43c87afd43aecf805ef50b79c98
nikhil soi <aanikhilsoni@gmail.com>
2020-05-23 16:05:42 +0530

    Updates

---
+++ precache-manifest.00778c3dada8d13bd1f5f6715d72748d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff1bf3123eb9e46ccbba9124bb5154b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b50ed311cac01eefca32",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "79d5ed1350149f488a66",
+    "url": "/portfolio/static/js/2.8836af42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8836af42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50ed311cac01eefca32",
+    "url": "/portfolio/static/js/main.4492fa4c.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "a1ac10f3ca004aae7d1ff9fab6c74543",
+    "url": "/portfolio/static/media/profile.a1ac10f3.jpg"
+  }
+]);
